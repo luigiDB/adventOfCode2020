151b7ffedc5a087feb0773123b7a256b21c214cd
luigidb <luigi.debianchi@gmail.com>
2020-12-13 10:10:11 +0100

    Real solution

--- src/main/java/exercises/Day13.java
+++ src/main/java/exercises/Day13.java
@@ -5,10 +5,7 @@ import org.jooq.lambda.tuple.Tuple;
 import org.jooq.lambda.tuple.Tuple2;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -46,35 +43,50 @@ public class Day13 {
                 .map(ferry -> Tuple.tuple(new BigInteger(ferry.v1), BigInteger.valueOf(ferry.v2)))
                 .collect(Collectors.toList());
 
-        Tuple2<BigInteger, BigInteger> max = ferries.stream()
-                .max(Comparator.comparing(a -> a.v1)).get();
+        List<Tuple2<BigInteger, BigInteger>> sorted = Seq.seq(ferries)
+                .sorted(Comparator.comparing(Tuple2::v1))
+                .reverse()
+                .collect(Collectors.toList());
 
-        Optional<BigInteger> first = generateSequence(max.v1, startMultiplier)
-                .findFirst(elem -> checkIfSequenceStartingHereIsValid(ferries, elem, max.v2));
-
-        if (first.isPresent())
-            return first.get().subtract(max.v2);
-        throw new UnsupportedOperationException();
-    }
-
-    private static boolean checkIfSequenceStartingHereIsValid(List<Tuple2<BigInteger, BigInteger>> ferries,
-                                                              BigInteger elem,
-                                                              BigInteger pivot) {
-        for (int i = 0; i < ferries.size(); i++) {
-            Tuple2<BigInteger, BigInteger> ferry = ferries.get(i);
-            BigInteger displacement = pivot.subtract(ferry.v2);
-            if (elem.subtract(displacement).mod(ferry.v1).compareTo(BigInteger.ZERO) != 0)
-                return false;
-            if (i == ferries.size() - 1)
-                return true;
+        Tuple2<BigInteger, BigInteger> pop = sorted.remove(0);
+        for (int i = 0; i < sorted.size(); i++) {
+            Tuple2<BigInteger, BigInteger> elem = sorted.get(i);
+            sorted.set(i, Tuple.tuple(elem.v1, elem.v2.subtract(pop.v2)));
         }
-        throw new UnsupportedOperationException();
+
+        BigInteger current = BigInteger.ZERO;
+        BigInteger increment = pop.v1;
+        BigInteger solution = null;
+
+        while (true) {
+            current = current.add(increment);
+            Tuple2<Optional<BigInteger>, BigInteger> result = checkIfCurrentIsValid(sorted, current, increment);
+            increment = result.v2;
+            if (result.v1.isPresent()) {
+                solution = result.v1.get();
+                break;
+            }
+        }
+        return solution.subtract(pop.v2);
     }
 
-    private static Seq<BigInteger> generateSequence(BigInteger start, long startMultiplier) {
-        return Seq.iterateWhilePresent(
-                start.multiply(BigInteger.valueOf(startMultiplier)),
-                current -> Optional.of(current.add(start))
-        );
+    private static Tuple2<Optional<BigInteger>, BigInteger> checkIfCurrentIsValid(List<Tuple2<BigInteger, BigInteger>> sorted,
+                                                              BigInteger current,
+                                                              BigInteger increment) {
+        List<Tuple2<BigInteger, BigInteger>> support = new ArrayList<>(sorted);
+        for (int i = 0; i < support.size(); i++) {
+            Tuple2<BigInteger, BigInteger> ferry = support.get(i);
+            if (current.add(ferry.v2).mod(ferry.v1).compareTo(BigInteger.ZERO) != 0) {
+                return Tuple.tuple(Optional.empty(), increment);
+            } else {
+                if (i == support.size() - 1) {
+                    return Tuple.tuple(Optional.of(current), increment);
+                } else {
+                    Tuple2<BigInteger, BigInteger> pop = sorted.remove(0);
+                    increment = increment.multiply(pop.v1);
+                }
+            }
+        }
+        return Tuple.tuple(Optional.empty(), increment);
     }
 }
