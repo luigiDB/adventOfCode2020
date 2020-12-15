3033b0b7865be06ddec0da2bbe8f16a227e09e07
luigidb <luigi.debianchi@gmail.com>
2020-12-15 09:02:20 +0100

    Refactor

--- src/main/java/exercises/Day15.java
+++ src/main/java/exercises/Day15.java
@@ -2,60 +2,46 @@ package exercises;
 
 import org.jooq.lambda.Seq;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day15 {
     public static long es1(Stream<String> input) {
-        Map<Long, Long> memory = initMemmory(input);
+        Map<Long, Long> memory = initMemory(input);
+        return game(memory, 2020);
+    }
 
+    public static long es2(Stream<String> input) {
+        Map<Long, Long> memory = initMemory(input);
+        return game(memory, 30000000);
+    }
+
+    private static long game(Map<Long, Long> memory, long turns) {
         long lastSpoken = 0;
-        lastSpoken = game(memory, lastSpoken, 2020);
-
+        for (long turn = memory.size(); turn < turns - 1; turn++) {
+            if (memory.containsKey(lastSpoken)) {
+                long newOccurrence = turn - memory.get(lastSpoken);
+                memory.put(lastSpoken, turn);
+                lastSpoken = newOccurrence;
+            } else {
+                memory.put(lastSpoken, turn);
+                lastSpoken = 0;
+            }
+//            System.out.println(lastSpoken);
+        }
         return lastSpoken;
     }
 
-    private static Map<Long, Long> initMemmory(Stream<String> input) {
-        String[] split = input
-                .collect(Collectors.toList())
-                .get(0)
-                .split(",");
-
+    private static Map<Long, Long> initMemory(Stream<String> input) {
         Map<Long, Long> memory = new HashMap<>();
 
-        Seq.seq(Arrays.stream(split)
+        Seq.seq(input
                 .map(Long::valueOf))
                 .zipWithIndex()
                 .forEach(tuple -> {
                     memory.put(tuple.v1, tuple.v2);
                 });
         return memory;
-    }
-
-    private static long game(Map<Long, Long> memory, long lastSpoken, long turns) {
-        for (long turn = memory.size(); turn < turns - 1; turn++) {
-            if (memory.containsKey(lastSpoken)) {
-                long newOccurence = turn - memory.get(lastSpoken);
-                memory.put(lastSpoken, turn);
-                lastSpoken = newOccurence;
-            } else {
-                memory.put(lastSpoken, turn);
-                lastSpoken = 0;
-            }
-            System.out.println(lastSpoken);
-        }
-        return lastSpoken;
-    }
-
-    public static long es2(Stream<String> input) {
-        Map<Long, Long> memory = initMemmory(input);
-
-        long lastSpoken = 0;
-        lastSpoken = game(memory, lastSpoken, 30000000);
-
-        return lastSpoken;
     }
 }
--- src/test/java/exercises/Day15Test.java
+++ src/test/java/exercises/Day15Test.java
@@ -1,6 +1,7 @@
 package exercises;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,41 +13,46 @@ public class Day15Test {
 
     @Test
     public void es1Test() {
-        Assert.assertEquals(436, Day15.es1(parseStrings(testInput)));
+        Assert.assertEquals(436, Day15.es1(parseStrings(testInput, ",")));
     }
 
     @Test
     public void es1AdditionalTests() {
-        Assert.assertEquals(1, Day15.es1(parseStrings("1,3,2")));
-        Assert.assertEquals(10, Day15.es1(parseStrings("2,1,3")));
-        Assert.assertEquals(27, Day15.es1(parseStrings("1,2,3")));
-        Assert.assertEquals(78, Day15.es1(parseStrings("2,3,1")));
-        Assert.assertEquals(438, Day15.es1(parseStrings("3,2,1")));
-        Assert.assertEquals(1836, Day15.es1(parseStrings("3,1,2")));
+        Assert.assertEquals(1, Day15.es1(parseStrings("1,3,2", ",")));
+        Assert.assertEquals(10, Day15.es1(parseStrings("2,1,3", ",")));
+        Assert.assertEquals(27, Day15.es1(parseStrings("1,2,3", ",")));
+        Assert.assertEquals(78, Day15.es1(parseStrings("2,3,1", ",")));
+        Assert.assertEquals(438, Day15.es1(parseStrings("3,2,1", ",")));
+        Assert.assertEquals(1836, Day15.es1(parseStrings("3,1,2", ",")));
     }
 
     @Test
     public void es1() {
-        long x = Day15.es1(parseStrings(getTestInput("15")));
+        long x = Day15.es1(parseStrings(getTestInput("15"), ","));
         assertEquals(257, x);
         System.out.println(x);
     }
 
     @Test
     public void es2Test() {
-        assertEquals(175594, Day15.es2(parseStrings("0,3,6")));
-        assertEquals(2578, Day15.es2(parseStrings("1,3,2")));
-        assertEquals(3544142, Day15.es2(parseStrings("2,1,3")));
-        assertEquals(261214, Day15.es2(parseStrings("1,2,3")));
-        assertEquals(6895259, Day15.es2(parseStrings("2,3,1")));
-        assertEquals(18, Day15.es2(parseStrings("3,2,1")));
-        assertEquals(362, Day15.es2(parseStrings("3,1,2")));
+        assertEquals(175594, Day15.es2(parseStrings("0,3,6", ",")));
+    }
+
+    @Test
+    @Ignore
+    public void es2AdditionalTests() {
+        assertEquals(2578, Day15.es2(parseStrings("1,3,2", ",")));
+        assertEquals(3544142, Day15.es2(parseStrings("2,1,3", ",")));
+        assertEquals(261214, Day15.es2(parseStrings("1,2,3", ",")));
+        assertEquals(6895259, Day15.es2(parseStrings("2,3,1", ",")));
+        assertEquals(18, Day15.es2(parseStrings("3,2,1", ",")));
+        assertEquals(362, Day15.es2(parseStrings("3,1,2", ",")));
     }
 
     @Test
     public void es2() {
-        long x = Day15.es2(parseStrings(getTestInput("15")));
-//        assertEquals(1, x);
+        long x = Day15.es2(parseStrings(getTestInput("15"), ","));
+        assertEquals(8546398, x);
         System.out.println(x);
     }
 }
